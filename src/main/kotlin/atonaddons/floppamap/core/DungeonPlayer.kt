FloppaCoding/AtonAddons

commit cb411f049a66ad30235eb0f436808fb690bb3389

Reworked the dungeon scan  - removed the uniqueRooms List. It was redundant and weird to use. Effectively replaced by Room.isUnique which updates itself automatically.  Reworked RoomData to allow for variable properties that are synchronizable between tiles of the same room. Also allows for room values being written after the scan (e.g. puzzle names) without interferring with the room data from rooms.json.  Made the world scan no longer overwrite already existing tiles, but instead update their data.

src/main/kotlin/atonaddons/floppamap/core/Room.kt
@@ -1,15 +1,33 @@
 package atonaddons.floppamap.core
 
 import atonaddons.module.impl.render.MapRooms
+import atonaddons.floppamap.dungeon.Dungeon
 import java.awt.Color
 
 class Room(x: Int, z: Int, var data: RoomData) : Tile(x, z) {
 
-    var core = 0
+    constructor(x: Int, z: Int, configData: RoomConfigData) : this(x, z, RoomData(configData = configData))
+
+    /**
+     * Core of this tile. This property is currently unused.
+     */
+    var core: Int? = null
     var isSeparator = false
 
+    /**
+     * Marks this tile as "unique".
+     * Unique is only relevant for rooms consisting of multiple tiles. All 1x1 rooms are unique by default.
+     * For rooms with multiple tiles the first Tile in [Dungeon.dungeonList] will be the unique one.
+     * This corresponds to the most west and north tile of a room. (west prioritized over north)
+     * The unique tile is the one which has the checkmark on the map.
+     */
+    val isUnique: Boolean
+        get() {
+            return Dungeon.dungeonList.first { it is Room && !it.isSeparator && it.data === this.data } == this
+        }
+
     override val color: Color
-        get() = if (this.state == RoomState.UNKNOWN && !visited)
+        get() = if (this.state == RoomState.QUESTION_MARK && !visited)
                 MapRooms.colorUnexplored.value
             else when (data.type) {
             RoomType.UNKNOWN ->   MapRooms.colorUnexplored.value

src/main/kotlin/atonaddons/floppamap/dungeon/Dungeon.kt
@@ -63,13 +63,11 @@ object Dungeon {
     private var lastScanTime: Long = 0
     private var isScanning = false
     var fullyScanned = false
-    var fullyScannedRotation = false
 
     var hasRunStarted = false
     var inBoss = false
     // 6 x 6 room grid, 11 x 11 with connections
     val dungeonList = Array<Tile?>(121) { null }
-    val uniqueRooms = mutableListOf<Room>()
 
     /**
      * Contains all the teammates in the current dungeon.
@@ -77,9 +75,6 @@ object Dungeon {
      */
     val dungeonTeammates = mutableListOf<DungeonPlayer>()
 
-    // Used for chat info
-    val puzzles = mutableListOf<String>()
-    var trapType = ""
     var witherDoors = 0
 
     /**
@@ -159,9 +154,10 @@ object Dungeon {
     @SubscribeEvent
     fun onRoomChange(event: RoomChangeEvent) {
         if (event.newRoom == null) return
+        if (event.newRoom.data.type == RoomType.BOSS || event.newRoom.data.type == RoomType.REGION) return
         // Update all of the connected rooms and separators to visited.
         dungeonList.forEach {
-            if ((it is Room) && it.data.type != RoomType.UNKNOWN && it.data == event.newRoom.data){
+            if ((it is Room) && it.data.type != RoomType.UNKNOWN && it.data === event.newRoom.data){
                 it.visited = true
             }
         }
@@ -174,6 +170,7 @@ object Dungeon {
         val doorColumn = if (abs(event.newRoom.column - event.oldRoom.column) <= 2)
                 (event.newRoom.column + event.oldRoom.column) shr 1
             else return
+        if (doorRow < 0 || doorRow > 11 || doorColumn < 0 || doorColumn < 11) return
         (dungeonList[doorRow*11 + doorColumn] as? Door)?.let { door ->
             door.visited = true
         }
@@ -186,7 +183,6 @@ object Dungeon {
         hasRunStarted = false
         inBoss = false
         fullyScanned = false
-        fullyScannedRotation = false
     }
 
     private fun shouldScan() =
@@ -232,10 +228,7 @@ object Dungeon {
         dungeonTeammates.clear()
 
         dungeonList.fill(null)
-        uniqueRooms.clear()
 
-        puzzles.clear()
-        trapType = ""
         witherDoors = 0
         RunInformation.reset()
     }

src/main/kotlin/atonaddons/floppamap/dungeon/DungeonScan.kt
@@ -15,11 +15,11 @@ import net.minecraft.util.ResourceLocation
 
 object DungeonScan {
 
-    private val roomList: Set<RoomData> = try {
+    private val roomList: Set<RoomConfigData> = try {
         Gson().fromJson(
             mc.resourceManager.getResource(ResourceLocation(AtonAddons.RESOURCE_DOMAIN, "floppamap/rooms.json"))
                 .inputStream.bufferedReader(),
-            object : TypeToken<Set<RoomData>>() {}.type
+            object : TypeToken<Set<RoomConfigData>>() {}.type
         )
     } catch (e: JsonSyntaxException) {
         println("Error parsing FloppaMap room data.")
@@ -29,6 +29,10 @@ object DungeonScan {
         setOf()
     }
 
+    /**
+     * Scans the dungeon from the loaded chunks in the world and updates [Dungeon.dungeonList] based on that.
+     * When all chunks are loaded [Dungeon.fullyScanned] will be set to true afterwards.
+     */
     fun scanDungeon() {
         var allLoaded = true
 
@@ -41,11 +45,26 @@ object DungeonScan {
                     allLoaded = false
                     continue
                 }
-                if (isColumnAir(xPos, zPos)) continue
 
                 if (Dungeon.dungeonList[z * 11 + x]?.scanned == true) continue
-                getRoom(xPos, zPos, z, x)?.let {
-                    Dungeon.dungeonList[z * 11 + x] = it
+                getRoomFromWorld(xPos, zPos, z, x)?.let { newTile ->
+                    val oldTile = Dungeon.dungeonList[z * 11 + x]
+                    // When the tile is already scanned from the map item make sure to not overwrite it.
+                    // Instead just update the values.
+                    if (oldTile != null) {
+                        /*
+                         NOTE: The following check does not account for the case when newTile and oldTile
+                          are of different type. This should not happen and when it does the newTile is likely faulty.
+                         */
+                        if (oldTile is Room && newTile is Room) { // Rooms
+                            oldTile.data.configData = newTile.data.configData
+                            oldTile.core = newTile.core
+                        }else { // Doors
+                            oldTile.scanned = true
+                        }
+                    }else {
+                        Dungeon.dungeonList[z * 11 + x] = newTile
+                    }
                 }
             }
         }
@@ -54,35 +73,27 @@ object DungeonScan {
         }
     }
 
-    private fun getRoom(x: Int, z: Int, row: Int, column: Int): Tile? {
+    /**
+     * Creates a dungeon Tile instance from the World.
+     * Also takes care of combining the data of neighbouring rooms.
+     * This is achieved by scanning the blocks in the column specified by [x] and [z].
+     * Returns null when the column is air.
+     */
+    private fun getRoomFromWorld(x: Int, z: Int, row: Int, column: Int): Tile? {
+        if (isColumnAir(x, z)) return null
         val rowEven = row and 1 == 0
         val columnEven = column and 1 == 0
 
         return when {
             rowEven && columnEven -> {
-                getRoomData(x, z)?.let {
-                    Room(x, z, it).apply {
-                        // Quite inefficient as we are scanning the core twice
-                        // Ideally we would save the core from getting room data
-                        core = getCore(x, z)
+                val core = getCore(x, z)
+                getRoomConfigData(core)?.let { configData ->
+                    // Connect the tile to neighboring ones.
+                    val data = MapUpdate.getAndSynchDataFromNeighborOrNew(row, column) { RoomData(configData) }
+                    // Update the configData
+                    data.configData = configData
 
-
-                        val candidate = Dungeon.uniqueRooms.find { match -> match.data.name == data.name }
-                        // If room not in list, or a cell further towards south east is in the list replace it with this one
-                        if (candidate == null || !candidate.scanned || candidate.x > x || (candidate.z > z && candidate.x == x)) {
-                            Dungeon.uniqueRooms.remove(candidate)
-                            Dungeon.uniqueRooms.add(this)
-                            if (candidate == null) {
-                                when (data.type) {
-                                    RoomType.PUZZLE -> Dungeon.puzzles.add(data.name)
-                                    else -> {}
-                                }
-                            }
-                        }
-                        if (candidate != null) {
-                            visited = candidate.visited
-                        }
-                    }
+                    Room(x, z, data).apply { this.core = core }
                 }
             }
             !rowEven && !columnEven -> {
@@ -118,11 +129,11 @@ object DungeonScan {
         }
     }
 
-    fun getRoomData(x: Int, z: Int): RoomData? {
-        return getRoomData(getCore(x, z))
+    fun getRoomConfigData(x: Int, z: Int): RoomConfigData? {
+        return getRoomConfigData(getCore(x, z))
     }
 
-    private fun getRoomData(hash: Int): RoomData? {
+    private fun getRoomConfigData(hash: Int): RoomConfigData? {
         return roomList.find { hash in it.cores }
     }
 

src/main/kotlin/atonaddons/floppamap/dungeon/MapRender.kt
@@ -186,7 +186,7 @@ object MapRender: HudElement(
 
                 if (tile.state == RoomState.UNDISCOVERED && !tile.visited) continue
 
-                if (tile is Room && (tile in Dungeon.uniqueRooms || tile.state == RoomState.UNKNOWN) ) {
+                if (tile is Room && (tile.isUnique || tile.state == RoomState.QUESTION_MARK) ) {
 
                     val xOffset = (x shr 1) * (roomSize + connectorSize)
                     val yOffset = (y shr 1) * (roomSize + connectorSize)
@@ -238,7 +238,7 @@ object MapRender: HudElement(
                 RoomState.CLEARED -> defaultWhite
                 RoomState.GREEN -> defaultGreen
                 RoomState.FAILED -> defaultCross
-                RoomState.UNKNOWN -> {
+                RoomState.QUESTION_MARK -> {
                     if (!tile.visited)
                         defaultQuestion
                     else null
@@ -249,7 +249,7 @@ object MapRender: HudElement(
                 RoomState.CLEARED -> neuWhite
                 RoomState.GREEN -> neuGreen
                 RoomState.FAILED -> neuCross
-                RoomState.UNKNOWN -> {
+                RoomState.QUESTION_MARK -> {
                     if (!tile.visited)
                         neuQuestion
                     else null

commit 95556b5a6dfc5f269c2e843d297a33b4bf173eac

Rearranged Dungeon.dungeonLists sort order from row*11 + column to column*11 + row. This is the natural sort order of the dungeon, both used by hypixel for sorting things like puzzle names but also used by the scan. This should also fix a bug introduced in commit cb411f049a66ad30235eb0f436808fb690bb3389 which made Room.isUnique be wrong.

src/main/kotlin/atonaddons/floppamap/core/DungeonPlayer.kt
@@ -168,7 +168,7 @@ class DungeonPlayer(var player: EntityPlayer, var name: String,
         val x = (((mapX + 2 - MapUtils.startCorner.first) / MapUtils.coordMultiplier ).toInt() shr 5)
         val z = (((mapZ + 2 - MapUtils.startCorner.second) / MapUtils.coordMultiplier).toInt() shr 5)
         if (x<0 || x > 5 || z < 0 || z > 5) return null
-        return x * 2 + z * 22
+        return x * 22 + z * 2
     }
 
     /**

src/main/kotlin/atonaddons/floppamap/core/Room.kt
@@ -25,7 +25,7 @@ class Room(x: Int, z: Int, var data: RoomData) : Tile(x, z) {
      */
     val isUnique: Boolean
         get() {
-            return Dungeon.dungeonList.first { it is Room && !it.isSeparator && it.data.name == this.data.name } == this
+            return Dungeon.dungeonList.first { it is Room && !it.isSeparator && it.data === this.data } === this
         }
 
     val canHaveSecrets: Boolean

src/main/kotlin/atonaddons/floppamap/dungeon/Dungeon.kt
@@ -42,13 +42,44 @@ object Dungeon {
     var hasRunStarted = false
     var inBoss = false
     // 6 x 6 room grid, 11 x 11 with connections
+    /**
+     * This field stores all Tiles of the current dungeon in a 11x11 Array.
+     * To access a specific Tile [getDungeonTile] and [setDungeonTile] are generally the better option.
+     * <p>
+     * The fields in this array correspond to a 11x11 Grid over the Dungeon.
+     * This grid consists of  6 rows / columns for the Rooms and in between another 5 for connectors (Doors / separators
+     * for rooms consisting of more than one Tile). All the Rooms lay on an even row and column.
+     * The array index for a given column (corresponds to the tiles x coordinate) and
+     * row (corresponds to the tiles z coordinate) is 'column*11 + row'. This is the same sort order as used by Hypixel
+     * for Puzzle names on the tab list.
+     * </p>
+     */
     val dungeonList = Array<Tile?>(121) { null }
     /**
-     * Gets the corresponding value from [dungeonList] but first performs a check whether the indices are in range.
+     * Gets the corresponding Tile from [dungeonList] but first performs a check whether the indices are in range.
      */
-    fun getDungeonTile(row: Int, column: Int) : Tile?{
+    @JvmName("getDungeonTileDefault")
+    fun getDungeonTile(column: Int, row: Int) : Tile?{
+        return getDungeonTile<Tile>(column, row)
+    }
+
+    /**
+     * Gets the corresponding Tile from [dungeonList] but first performs a check whether the indices are in range.
+     * It is attempted to cast the Tile to [T], if not possible returns null.
+     */
+    fun <T : Tile> getDungeonTile(column: Int, row: Int) : T?{
         if (row !in 0..10 || column !in 0..10) return null
-        return dungeonList[row*11 + column]
+        @Suppress("UNCHECKED_CAST") // This cast is fine, but the compiler does not know this.
+        return (dungeonList[column*11 + row] as? T)
+    }
+
+    /**
+     * Sets the corresponding value from [dungeonList] but first performs a check whether the indices are in range.
+     */
+    fun setDungeonTile(column: Int, row: Int, tile: Tile?): Boolean{
+        if (row !in 0..10 || column !in 0..10) return false
+        dungeonList[column*11 + row] = tile
+        return true
     }
 
     /**
@@ -178,7 +209,7 @@ object Dungeon {
                 (event.newRoom.column + event.oldRoom.column) shr 1
             else return
         if (doorRow < 0 || doorRow > 11 || doorColumn < 0 || doorColumn < 11) return
-        (dungeonList[doorRow*11 + doorColumn] as? Door)?.let { door ->
+        getDungeonTile<Door>(doorColumn, doorRow)?.let { door ->
             door.visited = true
         }
     }
@@ -219,7 +250,7 @@ object Dungeon {
         }else {
             val x = ((mc.thePlayer.posX - startX + 15).toInt() shr 5)
             val z = ((mc.thePlayer.posZ - startZ + 15).toInt() shr 5)
-            dungeonList.getOrNull(x * 2 + z * 22)
+            dungeonList.getOrNull(x * 22 + z * 2)
         }
         if (room !is Room) return null
         return room

src/main/kotlin/atonaddons/floppamap/dungeon/DungeonScan.kt
@@ -37,19 +37,19 @@ object DungeonScan {
         var allLoaded = true
         var updateConnection = false
 
-        scan@ for (x in 0..10) {
-            for (z in 0..10) {
-                val xPos = Dungeon.startX + x * (Dungeon.roomSize shr 1)
-                val zPos = Dungeon.startZ + z * (Dungeon.roomSize shr 1)
+        scan@ for (column in 0..10) {
+            for (row in 0..10) {
+                val xPos = Dungeon.startX + column * (Dungeon.roomSize shr 1)
+                val zPos = Dungeon.startZ + row * (Dungeon.roomSize shr 1)
 
                 if (!mc.theWorld.getChunkFromChunkCoords(xPos shr 4, zPos shr 4).isLoaded) {
                     allLoaded = false
                     continue
                 }
 
-                if (Dungeon.dungeonList[z * 11 + x]?.scanned == true) continue
-                getRoomFromWorld(xPos, zPos, z, x)?.let { newTile ->
-                    val oldTile = Dungeon.dungeonList[z * 11 + x]
+                if (Dungeon.dungeonList[column * 11 + row]?.scanned == true) continue
+                getRoomFromWorld(xPos, zPos, column, row)?.let { newTile ->
+                    val oldTile = Dungeon.dungeonList[column * 11 + row]
                     // When the tile is already scanned from the map item make sure to not overwrite it.
                     // Instead just update the values.
                     if (oldTile != null) {
@@ -60,11 +60,11 @@ object DungeonScan {
                         if (oldTile is Room && newTile is Room) { // Rooms
                             oldTile.data.configData = newTile.data.configData
                             oldTile.core = newTile.core
-                        }else { // Doors
+                        } else { // Doors
                             oldTile.scanned = true
                         }
-                    }else {
-                        Dungeon.dungeonList[z * 11 + x] = newTile
+                    } else {
+                        Dungeon.dungeonList[column * 11 + row] = newTile
                         if (newTile is Room && newTile.data.type == RoomType.NORMAL) updateConnection = true
                     }
                 }
@@ -85,13 +85,13 @@ object DungeonScan {
      * This is achieved by scanning the blocks in the column specified by [x] and [z].
      * Returns null when the column is air.
      */
-    private fun getRoomFromWorld(x: Int, z: Int, row: Int, column: Int): Tile? {
+    private fun getRoomFromWorld(x: Int, z: Int, column: Int, row: Int): Tile? {
         if (isColumnAir(x, z)) return null
         val rowEven = row and 1 == 0
         val columnEven = column and 1 == 0
 
         return when {
-            rowEven && columnEven -> {
+            rowEven && columnEven -> { // Room
                 val core = getCore(x, z)
                 getRoomConfigData(core)?.let { configData ->
                     val data = RoomData(configData)
@@ -99,14 +99,14 @@ object DungeonScan {
                     Room(x, z, data).apply { this.core = core }
                 }
             }
-            !rowEven && !columnEven -> {
-                Dungeon.dungeonList[(row - 1) * 11 + column - 1]?.let {
+            !rowEven && !columnEven -> {  // possible separator (only for 2x2)
+                Dungeon.getDungeonTile(column - 1, row - 1)?.let {
                     if (it is Room) {
                         Room(x, z, it.data).apply { isSeparator = true }
                     } else null
                 }
             }
-            isDoor(x, z) -> {
+            isDoor(x, z) -> { // Door
                 val bState = mc.theWorld.getBlockState(BlockPos(x, 69, z))
                 val doorType = when {
                     bState.block == Blocks.coal_block -> DoorType.WITHER
@@ -118,8 +118,9 @@ object DungeonScan {
                 }
                 Door(x, z, doorType)
             }
-            else -> {
-                Dungeon.dungeonList[if (rowEven) row * 11 + column - 1 else (row - 1) * 11 + column]?.let {
+            else -> { // Possible separator
+                (if (rowEven) Dungeon.getDungeonTile(column - 1, row)
+                else Dungeon.getDungeonTile(column, row - 1))?.let {
                     if (it is Room) {
                         if (it.data.type == RoomType.ENTRANCE) {
                             Door(x, z, DoorType.ENTRANCE)

src/main/kotlin/atonaddons/floppamap/dungeon/MapRender.kt
@@ -118,9 +118,9 @@ object MapRender: HudElement(
 
         val connectorSize = roomSize shr 2
 
-        for (y in 0..10) {
-            for (x in 0..10) {
-                val tile = Dungeon.dungeonList[y * 11 + x] ?: continue
+        for (x in 0..10) {
+            for (y in 0..10) {
+                val tile = Dungeon.dungeonList[x * 11 + y] ?: continue
                 if (tile.state == RoomState.UNDISCOVERED && !tile.visited) continue
 
                 val xOffset = (x shr 1) * (roomSize + connectorSize)
@@ -179,10 +179,10 @@ object MapRender: HudElement(
 
         val connectorSize = roomSize shr 2
 
-        for (y in 0..10 step 2) {
-            for (x in 0..10 step 2) {
+        for (x in 0..10 step 2) {
+            for (y in 0..10 step 2) {
 
-                val tile = Dungeon.dungeonList[y * 11 + x] ?: continue
+                val tile = Dungeon.dungeonList[x * 11 + y] ?: continue
 
                 if (tile.state == RoomState.UNDISCOVERED && !tile.visited) continue
 

src/main/kotlin/atonaddons/floppamap/dungeon/MapUpdate.kt
@@ -107,7 +107,7 @@ object MapUpdate {
     private fun updatePuzzleNames() {
         val puzzles = Dungeon.dungeonList.filterIsInstance<Room>()
             .filter { room -> !room.isSeparator && room.data.type == RoomType.PUZZLE && room.state.revealed  }
-            .sortedBy { room -> room.column*11 + room.row }
+            .sortedBy { room -> room.column*11 + room.row } // This is probably redundant since this is already the sort order of dungeonList
         if (RunInformation.puzzles.size == puzzles.size) {
             RunInformation.puzzles.withIndex().forEach { (index, puzzlePair) -> puzzles[index].data.name = puzzlePair.first }
             unmappedPuzz = false
@@ -131,14 +131,14 @@ object MapUpdate {
         val centerOffset = (MapUtils.roomSize shr 1)
         val increment = (MapUtils.roomSize shr 1) + 2
 
-        for (x in 0..10) {
-            for (z in 0..10) {
-                var tile = Dungeon.dungeonList[z * 11 + x]
+        for (column in 0..10) {
+            for (row in 0..10) {
+                var tile = Dungeon.dungeonList[column * 11 + row]
 
                 //If room unknown try to get it from the map item.
                 if (tile == null || (tile.state == RoomState.QUESTION_MARK && !tile.scanned)) {
-                    getRoomFromMap(z, x, mapColors)?.let { newTile ->
-                        Dungeon.dungeonList[z * 11 + x] = newTile
+                    getRoomFromMap(column, row, mapColors)?.let { newTile ->
+                        Dungeon.dungeonList[column * 11 + row] = newTile
 
                         if (newTile is Room && newTile.data.type == RoomType.NORMAL) shouldConnectRooms = true
 
@@ -156,10 +156,10 @@ object MapUpdate {
                 }
 
                 // Scan the room centers on the map for check marks.
-                tile = Dungeon.dungeonList[z * 11 + x]
+                tile = Dungeon.dungeonList[column * 11 + row]
                 if (tile != null) {
-                    val centerX = startX + x * increment + centerOffset
-                    val centerZ = startZ + z * increment + centerOffset
+                    val centerX = startX + column * increment + centerOffset
+                    val centerZ = startZ + row * increment + centerOffset
                     if (centerX >= 128 || centerZ >= 128) continue
                     val newState = when (mapColors[(centerZ shl 7) + centerX].toInt()) {
                         0 -> RoomState.UNDISCOVERED
@@ -206,7 +206,7 @@ object MapUpdate {
     /**
      * Gets a dungeon tile from the map item.
      */
-    private fun getRoomFromMap(row: Int, column: Int, mapColors: ByteArray): Tile? {
+    private fun getRoomFromMap(column: Int, row: Int, mapColors: ByteArray): Tile? {
 
         val startX = MapUtils.startCorner.first
         val startZ = MapUtils.startCorner.second
@@ -248,7 +248,7 @@ object MapUpdate {
             }
             !rowEven && !columnEven -> { // possible separator (only for 2x2)
                 if(mapColors[(centerZ shl 7) + centerX].toInt() != 0){
-                    Dungeon.dungeonList[(row - 1) * 11 + column - 1]?.let {
+                    Dungeon.getDungeonTile(column-1, row-1)?.let {
                         if (it is Room) {
                             Room(xPos, zPos, it.data).apply { isSeparator = true }
                         } else null
@@ -258,7 +258,8 @@ object MapUpdate {
             else -> { // door or separator
                 // Check the "side" of the connector to see whether it is a connector
                 if (mapColors[( (if (rowEven) cornerZ else centerZ) shl 7) + (if (rowEven) centerX else cornerX)].toInt() != 0) { // separator
-                    Dungeon.dungeonList[if (rowEven) row * 11 + column - 1 else (row - 1) * 11 + column]?.let {
+                    (if (rowEven) Dungeon.getDungeonTile(column - 1, row)
+                    else Dungeon.getDungeonTile(column, row - 1))?.let {
                         if (it is Room) {
                             Room(xPos, zPos, it.data).apply { isSeparator = true }
                         } else null
@@ -287,12 +288,12 @@ object MapUpdate {
             if (tile !is Room) return@forEach
             if (tile.data.type != RoomType.NORMAL) return@forEach
             if (tile.isSeparator) return@forEach
-            val row = index / 11
-            val column = index % 11
+            val column = index / 11
+            val row = index % 11
 
             // If the tile is a room check neighboring tiles for data in the order left, top
-            val leftConnector = Dungeon.getDungeonTile(row, column-1) as? Room
-            val topConnector = Dungeon.getDungeonTile(row-1, column) as? Room
+            val leftConnector = Dungeon.getDungeonTile(column-1, row) as? Room
+            val topConnector = Dungeon.getDungeonTile(column, row-1) as? Room
             var finalData: RoomData? = null
             val bufferedDataTemporary: MutableSet<RoomData> = mutableSetOf()
             var gotDataFromLeft = false
@@ -300,7 +301,7 @@ object MapUpdate {
             // link the tile and connector to the correct data
             // this code could be compacted into a loop to reduce redundancy, but this form has better readability
             if (leftConnector?.isSeparator == true) {
-                val leftRoom = Dungeon.getDungeonTile(row, column-2) as? Room
+                val leftRoom = Dungeon.getDungeonTile(column-2, row) as? Room
                 if (leftRoom != null) {
                     gotDataFromLeft = true
                     finalData = leftRoom.data
@@ -310,7 +311,7 @@ object MapUpdate {
                 }
             }
             if (topConnector?.isSeparator == true) {
-                val topRoom = Dungeon.getDungeonTile(row-2, column) as? Room
+                val topRoom = Dungeon.getDungeonTile(column, row-2) as? Room
                 if (topRoom != null) {
                     if (gotDataFromLeft) {
                         bufferedDataTemporary.add(topRoom.data)